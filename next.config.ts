5ffc39434356a3583ff02e17c55ff7b1decb170b
Lee <lee.b.martin@gmail.com>
2025-06-20 16:49:37 -0500

    turning off lint during deployment for now

--- next.config.ts
+++ next.config.ts
@@ -13,6 +13,11 @@ const nextConfig: NextConfig = {
     // This helps with better source maps
     optimizePackageImports: ['lucide-react'],
   },
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
